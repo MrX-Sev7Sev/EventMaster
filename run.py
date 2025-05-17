2d2f9d56b51684c734f869d00441e44ef2935e1a
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-05-17 10:10:14 +0500

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
+import sys
+from flask import Flask
+from pathlib import Path
 from app import create_app
+from app import socketio
 
 app = create_app()
+sys.path.append(str(Path(__file__).parent))
+
+app = Flask(__name__)
+
+@app.route('/')  # <- Это обязательный маршрут для 127.0.0.1:5000/
+def home():
+    return "Привет! Это главная страница."
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
