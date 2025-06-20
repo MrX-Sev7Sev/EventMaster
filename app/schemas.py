9cf5142717e50e27b630f5db2a3ba3892b4e9324
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-20 22:47:12 +0500

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr, Field, validator
 from typing import Optional
 from datetime import datetime
+from app.schemas import UserSchema as User
 
 class UserBase(BaseModel):
     email: EmailStr
