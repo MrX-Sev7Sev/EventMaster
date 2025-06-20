f87f762670933cdc2d7f1c01b10b16d0d0b014f4
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-21 01:50:18 +0500

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,11 +1,13 @@
 from .auth import auth_bp
 from .users import users_bp
 from .games import games_bp
+from .data import data_bp
 
-__all__ = ['auth_bp', 'users_bp', 'games_bp']
+__all__ = ['auth_bp', 'users_bp', 'games_bp', 'data_bp']
 
 def register_blueprints(app):
     """Регистрирует все blueprints в приложении"""
+    app.register_blueprint(data_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(games_bp)
