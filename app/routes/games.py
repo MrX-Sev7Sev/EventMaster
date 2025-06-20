1670225e16569604d792c23ecd2c4bdc6afe5050
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-20 16:55:05 +0500

    Create games.py

---
+++ app/routes/games.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
+from .models import Game, db
+
+games_bp = Blueprint('games', __name__, url_prefix='/api/games')
+
+@games_bp.route('/', methods=['GET'])
+def get_games():
+    games = Game.query.all()
+    return jsonify([game.to_dict() for game in games]), 200
+
+@games_bp.route('/<int:game_id>', methods=['GET'])
+def get_game(game_id):
+    game = Game.query.get_or_404(game_id)
+    return jsonify(game.to_dict()), 200
+
+@games_bp.route('/<int:game_id>/join', methods=['POST'])
+@jwt_required()
+def join_game(game_id):
+    user_id = get_jwt_identity()
+    game = Game.query.get_or_404(game_id)
+    game.players.append(user_id)
+    db.session.commit()
+    return jsonify({"message": "Joined the game!"}), 200
