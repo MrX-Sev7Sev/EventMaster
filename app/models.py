edbccbb8a7c8c32330a1e17c1fbb3e9c0766a409
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-21 17:22:48 +0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db
+from .extensions import db
 from datetime import datetime
 
 db = SQLAlchemy()
